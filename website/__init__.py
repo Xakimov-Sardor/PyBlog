b08192a07997a9a2ef524f39d7f116b71abcbe4d
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-11-17 20:58:41 +0500

    created login manager, connected database

--- website/__init__.py
+++ website/__init__.py
@@ -4,16 +4,35 @@ from os import path, urandom
 from flask_login import LoginManager
 
 
+db = SQLAlchemy()
+
+DB_NAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = urandom(40)
+    app.config['SECRET_KEY'] = 'asdsadsadasfasdasda'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
+    from .models import User
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
 
 
     return app
