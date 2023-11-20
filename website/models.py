1724cb0a460d24b5e5ed7cc7930d886a6ae59091
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-11-20 22:22:24 +0500

    title column added, backref mean understanding

--- website/models.py
+++ website/models.py
@@ -8,10 +8,12 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(50))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    posts = db.relationship('Post', backref='user', passive_deletes=True)
+    posts = db.relationship('Post', backref='usercha', passive_deletes=True)  #backref is important part!!!
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)  #learn
+
     text = db.Column(db.Text, nullable=False) #nullable mean is if input is empty
 
     author = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
