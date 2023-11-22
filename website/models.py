4023765e4193466c8b40a76fbce03842e7faa64f
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-11-22 19:47:21 +0500

    foreignkey and relationship learned

--- website/models.py
+++ website/models.py
@@ -8,14 +8,25 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(50))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    # -------------------------------------------------------------------------------------------------------------
     posts = db.relationship('Post', backref='usercha', passive_deletes=True)  #backref is important part!!!
-
+    # Post sinfiga munosabat. Bu munosabat har bir foydalanuvchi ko'p postlarga ega bo'lishi mumkinligini anglatadi
+    # -------------------------------------------------------------------------------------------------------------
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)  #learn
     text = db.Column(db.Text, nullable=False) #nullable mean is if input is empty
+    # -------------------------------------------------------------------------------------------------------------
     author_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False) #please find more information for this line
+    # Foydalanuvchi sinfining id maydoniga havola qiluvchi tashqi kalit. Bu shuni anglatadiki, har bir post bitta foydalanuvchi bilan bog'lanishi kerak
+    # -------------------------------------------------------------------------------------------------------------
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+
+
+
+
     comments = db.relationship('Comment', backref='comentcha', passive_deletes=True)
 
 class Comment(db.Model):
