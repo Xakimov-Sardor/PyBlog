00066e96240d360226d1a498fab23df9b36221e8
Xakimov-Sardor <sardor.xakimov.dev@yandex.ru>
2023-11-23 20:15:19 +0500

    like model added and it is working

--- website/models.py
+++ website/models.py
@@ -12,6 +12,7 @@ class User(db.Model, UserMixin):
     posts = db.relationship('Post', backref='usercha', passive_deletes=True)  #backref is important part!!!
     # Post sinfiga munosabat. Bu munosabat har bir foydalanuvchi ko'p postlarga ega bo'lishi mumkinligini anglatadi
     # -------------------------------------------------------------------------------------------------------------
+    likes = db.relationship('Like', backref='likecha_user', passive_deletes=True)
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)  #learn
@@ -21,13 +22,8 @@ class Post(db.Model):
     # Foydalanuvchi sinfining id maydoniga havola qiluvchi tashqi kalit. Bu shuni anglatadiki, har bir post bitta foydalanuvchi bilan bog'lanishi kerak
     # -------------------------------------------------------------------------------------------------------------
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-
-
-
-
-
-
     comments = db.relationship('Comment', backref='comentcha', passive_deletes=True)
+    likes = db.relationship('Like', backref='likecha', passive_deletes=True)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,3 +31,10 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     author = db.Column(db.String)
+
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
+    author_id = db.Column(db.Integer(), db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
